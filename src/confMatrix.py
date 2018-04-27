71c262a206034e2918cca6b3ef0dd48bcab71d03
Daniel Park <danpark0605@gmail.com>
2018-04-26 22:03:18 -0400

    added precision, recall, f1 to conf.py

--- src/confMatrix.py
+++ src/confMatrix.py
@@ -16,6 +16,7 @@ def confuse(doc):
     return tags
 
 def main():
+
     print("Enter Predicted Labels File: ", end="")
     predicts = input()
     predictFile = open(predicts, 'r')
@@ -26,16 +27,69 @@ def main():
 
     output = open("outputFile", "w")
 
-
     predictTags = []
     trueTags = []
 
     predictTags = confuse(predictFile)
     trueTags = confuse(truthFile)
 
+    sum = 0.0
+    diag = 0.0
+    recall = 0.0
+    recallAvg = 0.0
+    prec = 0.0
+    precAvg = 0.0
+
+    tPos = 0.0
+    fPos = 0.0
+    fNeg = 0.0
+
     conf = confusion_matrix(trueTags, predictTags)
-    confPD = pd.DataFrame(conf)
-    confPD.to_excel("outputFile.xlsx", index=False)
+    (r,c) = np.shape(conf)
+
+    i = 0
+    for row in conf:
+        j = 0
+        output.write("\t")
+        for col in row:
+            sum += col
+            if i == j:
+                tPos = col
+                diag += col
+            else:
+                fNeg += col
+            j += 1
+            output.write(str(col)+ "\t")
+        recall = tPos / (tPos + fNeg)
+        recallAvg += recall
+        i+=1
+        output.write("\n")
+
+    i = 0
+    for col in np.transpose(conf):
+        j = 0
+        for row in col:
+            if i == j:
+                tPos = row
+            else:
+                fPos += row
+            j += 1
+        prec = tPos / (tPos + fPos)
+        precAvg += prec
+        i += 1
+
+    recallAvg = recallAvg / r
+    precAvg = precAvg / c
+    f1 = 2.0 * (recallAvg * precAvg) / (recallAvg + precAvg)
+
+    output.write("accuracy = " + str(diag/sum) + "\n")
+    output.write("count = " + str(sum) + "\n")
+    output.write("precision =  " + str(precAvg) + "\n")
+    output.write("recall = " + str(recallAvg) + "\n")
+    output.write("f1 = " + str(f1) + "\n")
+
+    #confPD = pd.DataFrame(conf)
+    #confPD.to_excel("outputFile.xlsx", index=False)
     #output.write(np.array2string(confusion_matrix(trueTags, predictTags)))
 
 if __name__ == "__main__":
@@ -48,4 +102,12 @@ parse each line
 tokenize the line
 take in the tags
 do conf matrix
+
+Accuracy = TP+TN/TP+FP+FN+TN
+Precision = TP/TP+FP
+Recall = TP/TP+FN
+F1 Score = 2*(Recall * Precision) / (Recall + Precision)
+
+rows: false negatives other than the row its in: it predicted not a, but it was a
+columns: false positive: it as predicted a, but wasnt a
 '''
