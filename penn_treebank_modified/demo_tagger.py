83d73cfc57bb367dd8978d9f6f349e7bffed0f74
Daniel Park <danpark0605@gmail.com>
2018-05-03 11:44:08 -0400

    made the demo tagger work without restarting every time

--- penn_treebank_modified/demo_tagger.py
+++ penn_treebank_modified/demo_tagger.py
@@ -72,14 +72,6 @@ for key in emit:
 
 end = time.time()
 
-
-print transition
-print "\n"
-print emit
-print "\n"
-print context
-print sum(context.itervalues())
-
 print "Train Time:", end - start, "(s) ellapsed."
 
 # Currently the testing of the program does not work because it might encounter words that are not in training corpus
@@ -87,112 +79,113 @@ print "Train Time:", end - start, "(s) ellapsed."
 
 maxnum = max(emit.itervalues()) + 3
 
-line = raw_input("\nEnter Line: ")
-start = time.time()
+while True:
+	line = raw_input("\nEnter Line: ")
+	start = time.time()
 
-words = line.strip().split(' ')
-#if len(words) == 1:
-#	result.write("\n")
-#	continue
-l = len(words)
-# print words
-# forward step
-best_score = defaultdict(float)
-best_edge = defaultdict(str)
-best_score["0 <s>"] = 0
-best_edge["0 <s>"] = None
-for i in range(0,l):
-	for prev in context:
-		for next in context:
-			#print prev, next, words[i].lower()
-			b_key = str(i) + " " + prev
-			t_key = prev + " " + next
-			e_key = next + " " + words[i].lower()
-			#if e_key not in emit:
-			#	e_key = next + " <unk>"
-			if b_key in best_score and t_key in transition:
-				if e_key in emit:
-					score = best_score[b_key] + transition[t_key] + emit[e_key]
-				else:
-					score = best_score[b_key] + transition[t_key] + maxnum
-				b_key_next = str(i+1) + " " + next
-				if b_key_next not in best_score:
-					best_score[b_key_next] = score
-					best_edge[b_key_next] = b_key
-				else:
-					if score < best_score[b_key_next]:
+	words = line.strip().split(' ')
+	#if len(words) == 1:
+	#	result.write("\n")
+	#	continue
+	l = len(words)
+	# print words
+	# forward step
+	best_score = defaultdict(float)
+	best_edge = defaultdict(str)
+	best_score["0 <s>"] = 0
+	best_edge["0 <s>"] = None
+	for i in range(0,l):
+		for prev in context:
+			for next in context:
+				#print prev, next, words[i].lower()
+				b_key = str(i) + " " + prev
+				t_key = prev + " " + next
+				e_key = next + " " + words[i].lower()
+				#if e_key not in emit:
+				#	e_key = next + " <unk>"
+				if b_key in best_score and t_key in transition:
+					if e_key in emit:
+						score = best_score[b_key] + transition[t_key] + emit[e_key]
+					else:
+						score = best_score[b_key] + transition[t_key] + maxnum
+					b_key_next = str(i+1) + " " + next
+					if b_key_next not in best_score:
 						best_score[b_key_next] = score
 						best_edge[b_key_next] = b_key
-for prev in context:
-	next = "</s>"
-	b_key = str(l) + " " + prev
-	t_key = prev + " </s>"
-	e_key = "</s> " + words[i].lower()
-	# current fix for keys not in emit. this should be modified by ngram smoothing techniques
-	#if e_key not in emit:
-	#	e_key = "</s> <unk>"
-	if b_key in best_score and t_key in transition:
-		if e_key in emit:
-			score = best_score[b_key] + transition[t_key] + emit[e_key]
-		else:
-			score = best_score[b_key] + transition[t_key] + maxnum
-		b_key_next = str(l+1) + " </s>"
-		if b_key_next not in best_score:
-			best_score[b_key_next] = score
-			best_edge[b_key_next] = b_key
-		else:
-			if score < best_score[b_key_next]:
+					else:
+						if score < best_score[b_key_next]:
+							best_score[b_key_next] = score
+							best_edge[b_key_next] = b_key
+	for prev in context:
+		next = "</s>"
+		b_key = str(l) + " " + prev
+		t_key = prev + " </s>"
+		e_key = "</s> " + words[i].lower()
+		# current fix for keys not in emit. this should be modified by ngram smoothing techniques
+		#if e_key not in emit:
+		#	e_key = "</s> <unk>"
+		if b_key in best_score and t_key in transition:
+			if e_key in emit:
+				score = best_score[b_key] + transition[t_key] + emit[e_key]
+			else:
+				score = best_score[b_key] + transition[t_key] + maxnum
+			b_key_next = str(l+1) + " </s>"
+			if b_key_next not in best_score:
 				best_score[b_key_next] = score
 				best_edge[b_key_next] = b_key
+			else:
+				if score < best_score[b_key_next]:
+					best_score[b_key_next] = score
+					best_edge[b_key_next] = b_key
 
-# backwards step
-tags = []
-next_edge = best_edge[str(l+1) + " </s>"]
-while next_edge != "0 <s>":
-	token = next_edge.split(' ')
-	position = token[0]
-	tag = token[1]
-	word = words[int(position) - 1]
-	word_pos = word + "/" + tag
+	# backwards step
+	tags = []
+	next_edge = best_edge[str(l+1) + " </s>"]
+	while next_edge != "0 <s>":
+		token = next_edge.split(' ')
+		position = token[0]
+		tag = token[1]
+		word = words[int(position) - 1]
+		word_pos = word + "/" + tag
 
 
-	if best_edge[next_edge] != "0 <s>" and word[0].isupper():
-		word_pos = word + "/NNP"
-	try:
-		if float(word) > float('-inf'):
-			word_pos = word + "/CD"
-	except:
-		a = 1
-	'''
-	if not word.isalnum() and len(word) < 3:
-		word_pos = word + "/" + word
-		if word == ';':
-			word_pos = word + "/:"
-	if word == '%':
-		word_pos = word + "/NN"
-	if word.isdigit():
-		word_post = word + "/CD"
-	if word == '\'s':
-		word_pos = word + "/POS"
-	if word.lower() in article:
-		word_pos = word + "/DT"
-	#if word.lower() in ppss:
-		#word_pos = word + "/ppss"
-	if word.lower() in pps:
-		word_pos = word + "/PPS"
-	if word.lower() in preposition:
-		word_pos = word + "/IN"
-	'''
-		#do nothing
-	#print word_pos
+		if best_edge[next_edge] != "0 <s>" and word[0].isupper():
+			word_pos = word + "/NNP"
+		try:
+			if float(word) > float('-inf'):
+				word_pos = word + "/CD"
+		except:
+			a = 1
+		'''
+		if not word.isalnum() and len(word) < 3:
+			word_pos = word + "/" + word
+			if word == ';':
+				word_pos = word + "/:"
+		if word == '%':
+			word_pos = word + "/NN"
+		if word.isdigit():
+			word_post = word + "/CD"
+		if word == '\'s':
+			word_pos = word + "/POS"
+		if word.lower() in article:
+			word_pos = word + "/DT"
+		#if word.lower() in ppss:
+			#word_pos = word + "/ppss"
+		if word.lower() in pps:
+			word_pos = word + "/PPS"
+		if word.lower() in preposition:
+			word_pos = word + "/IN"
+		'''
+			#do nothing
+		#print word_pos
 
-	tags.append(word_pos)
-	next_edge = best_edge[next_edge]
+		tags.append(word_pos)
+		next_edge = best_edge[next_edge]
 
-tags.reverse()
-print "Tagged Line: " + ' '.join(tags) + "\n"
+	tags.reverse()
+	print "Tagged Line: " + ' '.join(tags) + "\n"
 
+	end = time.time()
+	print "Test Time:", end-start, "(s) ellapsed.\n"
+	
 f.close()
-
-end = time.time()
-print "Test Time:", end-start, "(s) ellapsed."
